7a84c8b7edd62c9bb7897b63febdab5fc7fe705d
Bounteous <bounteous17@protonmail.com>
2019-04-14 12:48:39 +0200

    Open file decomposed

--- utils/functions.py
+++ utils/functions.py
@@ -2,8 +2,30 @@ import sys
 from ruamel.yaml import YAML
 
 def getConfig():
-    with open("./config/config.yaml", 'r') as yamlconfig:
+    try:
+        readYamlConfig = readFile("./config/config.yaml", 'r')
+        if "error" in readYamlConfig:
+            return readYamlConfig
+
+        yamlconfig = readYamlConfig
         config = YAML().load(yamlconfig)
-    return config
+        return config
+    except Exception as e:
+        print (e)
+        return {
+                    "error": "An unexpected error ocurred reading config"
+                }
+
+def readFile(path, perms):
+    try:
+        if not perms:
+            perms = 'r'
+        f = open(path, perms)
+        return f.read()
+    except Exception as e:
+        print (e)
+        return {
+                    "error": "An unexpected error occurred reading file"
+                }
 
 Config = getConfig()
