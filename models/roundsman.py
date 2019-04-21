906daed3f4ca2957afe5b427cd9b26087c8595f8
Bounteous <bounteous17@protonmail.com>
2019-04-21 12:58:24 +0200

    Users mongo model

---
+++ models/roundsman.py
@@ -0,0 +1,14 @@
+from mongoengine import *
+import datetime
+
+class RoundsMan(Document):
+    email = StringField(required=True, max_length=50, unique=True)
+    password = StringField(required=True, min_length=8, max_length=30)
+    dni = StringField(required=True, length=9, unique=True)
+    name = StringField(required=True, max_length=50)
+    surname = StringField(required=True, max_length=50)
+    age = IntField(required=True, min_value=18, max_value=99)
+    city = StringField(required=True, max_length=50)
+    street = StringField(required=True, max_length=50)
+    post_code = IntField(required=True, min_value=0, max_value=99999)
+    date = DateTimeField(default=datetime.datetime.now)
