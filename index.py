373d462c6ea72b5e1b8b97fc465f0d752417a686
Bounteous <bounteous17@protonmail.com>
2019-04-21 12:59:57 +0200

    Signup route

--- index.py
+++ index.py
@@ -1,11 +1,15 @@
 #!flask/bin/python
 # Module imports
-from flask import Flask, request, jsonify
+from flask import Flask, request,  abort, jsonify
 from utils import functions as _functions
 # Routes imports
-from routes.public import status as _status
+from routes.public import status as _status, signup as _signup
 # Models
 from models import rest as _rest
+# Modules
+from modules.db import db as _db
+
+_db.instance()
 
 app = Flask(__name__) # Create http server
 _Config = _functions.Config
@@ -21,5 +25,15 @@ def alive():
         return _functions.setFlaskResponse(_status.alive())
     except Exception as e:
         return _functions.setFlaskResponse(_functions.setModuleError(payload=e, error="Error into /"))
-    
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    try:
+        if not request.json:
+            abort(400)
+        return _functions.setFlaskResponse(_signup.signup(request.json))
+    except Exception as e:
+        return _functions.setFlaskResponse(_functions.setModuleError(payload=e, error="Error into /signup"))
+
+
 app.run()
