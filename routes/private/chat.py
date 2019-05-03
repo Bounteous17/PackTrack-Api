ff5f3a12dd12c357eaf9092305cc27952a03d809
Bounteous <bounteous17@protonmail.com>
2019-05-03 17:45:58 +0200

    New chat endpoint

--- routes/private/chat.py
+++ routes/private/chat.py
@@ -1,16 +1,32 @@
-from utils import functions as _functions
-from models import chat as _modelChat
+from flask_jwt_extended import jwt_required, get_jwt_identity
 from flask_restful import Resource, reqparse
+from utils import functions as _functions, responses as _responses
+from models import chat as _modelChat
+from modules.user import user as _mUser
 
 parser = reqparse.RequestParser()
 parser.add_argument('username', help = 'User with whom to start the chat', required = True)
 
 class Chat(Resource):
     #create new chat
+    @jwt_required
     def post(self):
         try:
             reqData = parser.parse_args()
-            newChat = _modelChat.Chat()
+            alloweds = []
+            sUser = _mUser.findOne('username', reqData['username'])
+            if _functions.resultError(sUser):
+                return sUser.flaskResp()
+            if sUser is None:
+                return _responses.userNotFound().flaskResp()
+            print (sUser)
+            alloweds.append(sUser._id)
+            creator = get_jwt_identity()
+            alloweds.append(creator)
+            newChat = _modelChat.Chat(
+                    creator=creator,
+                    alloweds=alloweds
+            )
             newChat.save()
             return _functions.setModuleSuccess(payload='Chat initialized correctly', status=200).flaskResp()
         except Exception as e:
--- routes/private/user.py
+++ routes/private/user.py
@@ -2,7 +2,7 @@ from flask_restful import Resource, reqparse
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from modules.user import user as _mUser
 from models import rest as _rest
-from utils import functions as _functions
+from utils import functions as _functions, responses as _responses
 parser = reqparse.RequestParser()
 
 class Info(Resource):
@@ -12,7 +12,7 @@ class Info(Resource):
             userId = get_jwt_identity()
             sUser = _mUser.findById(userId)
             if sUser is None:
-                return _functions.setModuleError(payload='User not found for jwt provided', error='User not found ...', status=404).flaskResp()
+                return _responses.userNotFound().flaskResp()
             if _functions.resultError(sUser):
                 return sUser.flaskResp()
             return _functions.setModuleSuccess(payload=_mUser.selectInfo(['email', 'dni', 'name', 'surname', 'age'], sUser), key='master', status=200).flaskResp()
