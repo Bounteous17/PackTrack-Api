33bb96a1aea5b08b42e8660b3592a651b5d3d386
Bounteous <bounteous17@protonmail.com>
2019-05-03 17:46:48 +0200

    Refactor find users functions

--- models/chat.py
+++ models/chat.py
@@ -1,7 +1,10 @@
 from mongoengine import *
 import datetime
+from models import user as _modelUser
 
 class Chat(Document):
     _id = ObjectIdField()
+    creator = ReferenceField(_modelUser.User)
     messages = ListField(default=[])
+    alloweds = ListField(ReferenceField(_modelUser.User), default=[])
     created = DateTimeField(default=datetime.datetime.now)
--- modules/user/user.py
+++ modules/user/user.py
@@ -10,7 +10,10 @@ def findById(userId):
 
 def findOne(field, value):
     try:
-        return _user.User.objects.get(**{'{}'.format(field): value})
+        condition = {'{}'.format(field): value}
+        if _user.User.objects.filter(** condition).count() > 0:
+            return _user.User.objects.get(** condition)
+        return _functions.setModuleError(payload='User not found on users collection', error='User not found ...', status=404)
     except Exception as e:
         return _functions.setModuleError(payload=e, error='Error find user ...', status=500)
 
