72a43204228a0bf1e293f7a15079c4b878308a70
Bounteous <bounteous17@protonmail.com>
2019-05-04 11:57:59 +0200

    List my chats route

--- routes/private/chat.py
+++ routes/private/chat.py
@@ -1,11 +1,13 @@
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from flask_restful import Resource, reqparse
 from utils import functions as _functions, responses as _responses
-from models import chat as _modelChat
+from models import models as _models
 from modules.user import user as _mUser
+from modules.chat import chat as _mChat
 
 parser = reqparse.RequestParser()
-parser.add_argument('username', help = 'User with whom to start the chat', required = True)
+parser.add_argument('username', help = 'User with whom to start the chat', required = False)
+parser.add_argument('chat', help = 'Chat id', required = False)
 
 class Chat(Resource):
     #create new chat
@@ -17,17 +19,36 @@ class Chat(Resource):
             sUser = _mUser.findOne('username', reqData['username'])
             if _functions.resultError(sUser):
                 return sUser.flaskResp()
-            if sUser is None:
-                return _responses.userNotFound().flaskResp()
-            print (sUser)
             alloweds.append(sUser._id)
             creator = get_jwt_identity()
             alloweds.append(creator)
-            newChat = _modelChat.Chat(
+            newChat = _models.Chat(
                     creator=creator,
                     alloweds=alloweds
             )
             newChat.save()
+            uUser = _mUser.findOneAndUpdate('_id', creator, 'chats', newChat.id)
+            if _functions.resultError(uUser):
+                return uUser.flaskResp()
+            
             return _functions.setModuleSuccess(payload='Chat initialized correctly', status=200).flaskResp()
         except Exception as e:
             return _functions.setModuleError(payload=e, error='Error creating new chat, try it later ...', status=500).flaskResp()
+
+    #list user chats
+    @jwt_required
+    def get(self):
+        try:
+            reqData = parser.parse_args()
+            user = get_jwt_identity()
+            sUser = _mUser.findOne('_id', user)
+            if _functions.resultError(sUser):
+                return sChat.flaskResp()
+            userChats = _mUser.myChats(user)
+            if _functions.resultError(sUser._id):
+                return userChats.flaskResp()
+            return _functions.setModuleSuccess(payload=userChats, key='list', status=200).flaskResp()
+        except Exception as e:
+            return _functions.setModuleError(payload=e, error='Error listing chats, try it later ...', status=500).flaskResp()
+
+
