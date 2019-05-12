4689620db18396fd8f7a213dae9118a4f60e7d43
Bounteous <bounteous17@protonmail.com>
2019-05-12 20:20:04 +0200

    Aggregate user chats list

--- modules/user/user.py
+++ modules/user/user.py
@@ -3,42 +3,77 @@ from models import models as _models
 from modules.db import utils as _mDbUtils
 import bson
 
+
 def myChats(userId):
     try:
         userChats = list(_models.User.objects.aggregate(*[
-                    { '$match': { '_id': _mDbUtils.hexObjId(userId) } },
-                    { '$unwind': '$chats' },
-                    {
-                        '$lookup':{
-                            'from': _models.Chat._get_collection_name(),
-                            'localField': 'chats',
-                            'foreignField': '_id',
-                            'as': 'chatObjects'
-                        }
-                    },
-                    { '$unwind': '$chatObjects' },
-                    { "$group": {
-                        "_id": "$_id",
-                        "chats": { "$push": "$chatObjects" }
-                    }}
-                ]))[0]
-        return userChats['chats']
+            {'$match': {'_id': _mDbUtils.hexObjId(userId)}},
+            {
+                '$lookup': {
+                    'from': _models.Chat._get_collection_name(),
+                    'localField': 'chats',
+                    'foreignField': '_id',
+                    'as': 'chatObjects'
+                }
+            },
+            {'$unwind': {'path': '$chatObjects', 'preserveNullAndEmptyArrays': True}},
+            {
+                '$lookup': {
+                    'from': _models.User._get_collection_name(),
+                    'localField': 'chatObjects.creator',
+                    'foreignField': '_id',
+                    'as': 'chatObjects.creator'
+                }
+            },
+            {
+                '$lookup': {
+                    'from': _models.User._get_collection_name(),
+                    'localField': 'chatObjects.alloweds',
+                    'foreignField': '_id',
+                    'as': 'chatObjects.alloweds'
+                }
+            },
+            {'$group': {
+                '_id': "$_id",
+                'chats': {"$push": "$chatObjects"}
+            }},
+        ]))[0]['chats']
+        return myChatsClear(userChats)
     except Exception as e:
         return _functions.setModuleError(payload=e, error='Error listing user chats ...', status=500)
+
+
+def myChatsClear(aggregateObj):
+    try:
+        userUnwantedRes = _responses.unwantedRes('user')
+        for count, chat in enumerate(aggregateObj):
+            aggregateObj[count]['creator'] = aggregateObj[count]['creator'][0]
+            [aggregateObj[count]['creator'].pop(
+                _pull) for _pull in userUnwantedRes]
+            for u_count, allowed in enumerate(aggregateObj[count]['alloweds']):
+                [aggregateObj[count]['alloweds'][u_count].pop(
+                    _pull) for _pull in userUnwantedRes]
+
+        return aggregateObj
+    except Exception as e:
+        return _functions.setModuleError(payload=e, error='Error cleaning user chats list ...', status=500)
+
 
 def checkExists(field, value):
     try:
         return _models.User.objects.get(** {'{}'.format(field): value})
     except _models.User.DoesNotExist:
-       return False
+        return False
     except Exception as e:
         return _functions.setModuleError(payload=e, error='Error check user exists ...', status=500)
 
+
 def findById(userId):
     try:
-        return _models.User.objects.get(_id = userId)
+        return _models.User.objects.get(_id=userId)
     except Exception as e:
         return _functions.setModuleError(payload=e, error='Error find user ...', status=500)
+
 
 def findOne(field, value):
     try:
@@ -51,6 +86,7 @@ def findOne(field, value):
         return _models.User.objects.get(** condition)
     except Exception as e:
         return _functions.setModuleError(payload=e, error='Error find user ...', status=500)
+
 
 def findOneAndUpdate(field, value, uField, uValue):
     try:
